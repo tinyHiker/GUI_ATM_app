2165b48979f89d8864686c0997445eb088ba4a29
Taha Iqbal <tiqbal10b@gmail.com>
2023-12-21 21:21:47 -0500

    Complete personal detail, deposit, and withdraw functions. Update app with customtkinter

--- main.py
+++ main.py
@@ -1,9 +1,160 @@
-from tkinter import *
+import customtkinter
+from customtkinter import *
 import os
-from PIL import ImageTk, Image
+import time
 
-#Main Screen
+customtkinter.set_appearance_mode("dark")
 
+def register():
+    
+    # Vars
+    global temp_name, temp_age, temp_gender, temp_password, notif, register_screen
+    
+    
+    temp_name = StringVar()
+    temp_age = StringVar()
+    temp_gender = StringVar()
+    temp_password = StringVar()
+    
+    
+    register_screen = CTkToplevel(master)
+    register_screen.geometry('500x500')
+    register_screen.title('Register')
+    
+    #Labels
+    register_prompt = CTkLabel(master = register_screen, text = "Please enter your details below to register:", font = ('Georgia', 17))
+    register_prompt.place(x= 70, y=90)
+    
+    
+    name_label = CTkLabel(master = register_screen, text = "Name: ", font = ('Georgia', 15))
+    name_label.place(x= 100, y= 135)
+    
+    age_label = CTkLabel(master = register_screen, text = "Age: ", font = ('Georgia', 15))
+    age_label.place(x= 108, y= 175)
+    
+   
+    gender_label = CTkLabel(master = register_screen, text = "Gender: ", font = ('Georgia', 15))
+    gender_label.place(x= 95, y= 215)
+    
+    password_label = CTkLabel(master = register_screen, text = "Password: ", font = ('Georgia', 15))
+    password_label.place(x= 83, y= 255)
+    
+    notif = CTkLabel(master = register_screen, text='', font = ('Georgia', 16))
+    notif.place(x = 155, y = 375)
+    
+    #Entries
+    CTkEntry(register_screen, textvariable= temp_name).place(x= 155, y= 135)
+    CTkEntry(register_screen, textvariable= temp_age).place(x= 155, y= 175)
+    CTkEntry(register_screen, textvariable= temp_gender).place(x= 155, y= 215)
+    CTkEntry(register_screen, textvariable= temp_password, show = "*").place(x= 155, y= 255)
+    CTkButton(register_screen, text = "Register", command = finish_reg, font = ("Georgia", 14)).place(x=155, y= 325)
+    
+    
+def finish_reg():
+    name = temp_name.get()
+    age = temp_age.get()
+    gender = temp_gender.get()
+    password = temp_password.get()
+    all_accounts = os.listdir()
+    print(all_accounts)
+    
+    if name == "" or age == "" or gender == "" or password == "":
+        notif.configure(text_color= 'red', text = 'All fields required')
+        return 
+    
+    for name_check in all_accounts:
+        if name == name_check:
+            notif.configure(text_color="red", text="Account already exists")
+            return
+        else:
+            with open(name, 'w') as new_file:
+                new_file.write(name + '\n')
+                new_file.write(password + '\n')
+                new_file.write(age + '\n')
+                new_file.write(gender+ '\n')
+                new_file.write('0')
+                new_file.close()
+                notif.configure(text_color = 'green', text = "Account created succesfully\nGo to the login page")
+                
+                
+    
+    
+def login():
+    
+    global temp_login_name, temp_login_password, login_notif, login_screen
+    
+    
+    temp_login_name = StringVar()
+    temp_login_password = StringVar()
+    
+    #Login Screen
+    login_screen = CTkToplevel(master)
+    login_screen.geometry('500x500')
+    login_screen.title("Login")
+    #####################################
+    
+    #Labels
+    login_prompt = CTkLabel(master = login_screen, text = "Please enter your credentials below to login", font = ('Georgia', 17))
+    login_prompt.place(x= 70, y=110)
+    
+    
+    username_label = CTkLabel(master = login_screen, text = "Username: ", font = ('Georgia', 15))
+    username_label.place(x= 100, y= 155)
+    
+    user_password_label = CTkLabel(master = login_screen, text = "Password: ", font = ('Georgia', 15))
+    user_password_label.place(x= 108, y= 195)
+    
+   
+    login_notif = CTkLabel(master = login_screen, text='', font = ('Georgia', 16))
+    login_notif.place(x = 155, y = 340)
+    
+    
+    #Entries
+    CTkEntry(login_screen, textvariable= temp_login_name).place(x= 185, y= 155)
+    CTkEntry(login_screen, textvariable= temp_login_password).place(x= 185, y= 195)
+    
+    CTkButton(login_screen, text = "Login", command = login_session, font = ("Georgia", 14)).place(x=155, y= 265)
+    
+    
+    
+def login_session():
+    global login_name
+    all_accounts= os.listdir()
+    login_name = temp_login_name.get()
+    login_password= temp_login_password.get()
+    
+    for name in all_accounts:
+        if login_name == name:
+            with open(name, 'r') as file:
+                file_data = file.read()
+                file_data = file_data.split('\n')
+                password = file_data[1]
+                
+                #Account Dashboard
+                if login_password == password:
+                    login_screen.destroy()
+                    account_dashboard = CTkToplevel(master)
+                    account_dashboard.title('Dashboard')
+                    account_dashboard.geometry('500x500')
+                    
+                    #Labels
+                    CTkLabel(account_dashboard, text = "Account Dashboard", font=('Georgia', 22)).place(x= 153, y= 30)
+                    CTkLabel(account_dashboard, text = "Welcome " + name, font=('Georgia', 14)).place(x= 200, y= 75)
+                    
+                    
+                    #Buttons
+                    CTkButton(account_dashboard, text = "Personal Details", font = ('Georgia', 16), width = 40, height = 30, command = personal_details).place(x= 185, y= 160)
+                    CTkButton(account_dashboard, text = "Deposit", font = ('Georgia', 16), width = 40, height = 30, command = deposit).place(x= 215,y = 250)
+                    CTkButton(account_dashboard, text = "Withdraw", font = ('Georgia', 16), width = 40, height = 30, command = withdraw).place(x= 207, y= 350)
+                    
+                    return 
+                else:
+                    login_notif.configure(text_color = "red", text='Invalid password')
+                    
+                return 
+        
+    login_notif.configure(text_color = "red", text='Invalid username')
+    
 def personal_details():
     print("personal details")
     
@@ -17,169 +168,157 @@ def personal_details():
     details_balance = user_details[4]
     
     #Personal Details Screen
-    personal_details_screen = Toplevel(master)
+    personal_details_screen = CTkToplevel(master)
+    personal_details_screen.geometry('500x500')
+    
     personal_details_screen.title('Personal Details')
     
     #Labels
-    Label(personal_details_screen, text = "Perosnal Details" , font = ('Calibri', 12)).grid(row=0, sticky =N , pady=10)
-    Label(personal_details_screen, text = "Name: " + details_name, font = ('Calibri', 12)).grid(row=1, sticky =W)
-    Label(personal_details_screen, text = "Age: " + details_age, font = ('Calibri', 12)).grid(row=2, sticky =W)
-    Label(personal_details_screen, text = "Gender: " + details_gender, font = ('Calibri', 12)).grid(row=3, sticky =W)
-    Label(personal_details_screen, text = "Balance: $" + details_balance, font = ('Calibri', 12)).grid(row=4, sticky =W)
-    
-    Label
-
-def deposit():
-    print("deposit")
-
-def withdraw():
-    print('withdraw')
-
-def login_session():
-    global login_name
-    all_accounts= os.listdir()
-    login_name = temp_login_name.get()
-    login_password= temp_login_password.get()
-    
-    for name in all_accounts:
-        if login_name == name:
-            with open(name, 'r') as file:
-                file_data = file.read()
-                file_data = file_data.split('\n')
-                password= file_data[1]
-                
-                #Account Dashboard
-                if login_password == password:
-                    login_screen.destroy()
-                    account_dashboard = Toplevel(master)
-                    account_dashboard.title('Dashboard')
-                    
-                    #Labels
-                    Label(account_dashboard, text = "Account Dashboard", font=('Calibri', 12)).grid(row=0, sticky = N, pady =10) # TIMESTAMP: 32:37 VID 2
-                    Label(account_dashboard, text = "welcome " + name, font=('Calibri', 12)).grid(row=1, sticky = N, pady =5)
-                    
-                    #Buttons
-                    Button(account_dashboard, text = "Personal Details", font = ('Calibri', 12), width = 30, command = personal_details).grid(row=2, sticky =N , padx=10)
-                    Button(account_dashboard, text = "Deposit", font = ('Calibri', 12), width = 30, command = deposit).grid(row=3, sticky =N , padx=10)
-                    Button(account_dashboard, text = "Withdraw", font = ('Calibri', 12), width = 30, command = withdraw).grid(row=4, sticky =N , padx=10)
-                    Label(account_dashboard).grid(row=5, sticky = N, pady =10)
-                    return 
-                else:
-                    login_notif.config(fg='red', text='Invalid password')
-                    
-                return 
-        
-    login_notif.config(fg='red', text='Invalid username')
-    
-            
-            
-            
-            
-    
-
-def login():
-    
-    global temp_login_name, temp_login_password, login_notif, login_screen
-    
-    
-    temp_login_name = StringVar()
-    temp_login_password = StringVar()
-    
-    #Login Screen
-    login_screen = Toplevel(master)
-    login_screen.title("Login")
-    
-    #Labels
-    Label(login_screen, text = "Login to your account", font = ('Calibri', 12)).grid(row=0, sticky=N, pady = 10 )
-    Label(login_screen, text = "Username", font = ('Calibri', 12)).grid(row=1, sticky=W, pady=10)
-    Label(login_screen, text = "Password", font = ('Calibri', 12)).grid(row=2, sticky=W, pady=10)
-    login_notif = Label(login_screen, text= '',  font = ('Calibri', 12))
-    login_notif.grid(row=4, sticky = N)
-    
-    #Entry
-    Entry(login_screen, textvariable=temp_login_name).grid(row=1, column=1, padx=5)
-    Entry(login_screen, textvariable=temp_login_password, show='*').grid(row=2, column=1, padx=5)
-    
-    #Button
-    Button(login_screen, text="Login", command = login_session, width=15, font = ('Calibri', 12)).grid(row=3, sticky=W, pady=5, padx=5)
+    CTkLabel(personal_details_screen, text = "Personal Details" , font = ('Georgia', 24)).place(x= 170, y= 50)
+    CTkLabel(personal_details_screen, text = "Name: " + details_name, font = ('Georgia', 16)).place(x= 200, y= 140)
+    CTkLabel(personal_details_screen, text = "Age: " + details_age, font = ('Georgia', 16)).place(x=200, y= 210)
+    CTkLabel(personal_details_screen, text = "Gender: " + details_gender, font = ('Georgia', 16)).place(x=200, y= 280)
+    CTkLabel(personal_details_screen, text = "Balance: $" + details_balance, font = ('Georgia', 16)).place(x=200, y= 350)
 
 
-
-def finish_reg():
-    print('done')
-    name = temp_name.get()
-    age = temp_age.get()
-    gender = temp_gender.get()
-    password = temp_password.get()
-    all_accounts = os.listdir()
-    print(all_accounts)
-    
-    if name == "" or age == "" or gender == "" or password == "":
-        notif.config(fg= 'red', text = 'All fields required')
+def finish_deposit():
+    if amount.get() == "":
+        deposit_notif.config(text = 'Amount is required!', fg = 'red')
+        return 
+    if float(amount.get()) <= 0:
+        deposit_notif.config(text = "Negative currency is not accepted", fg = "red")
         return 
     
-    for name_check in all_accounts:
-        if name == name_check:
-            notif.config(fg="red", text="Account already exists")
-            return
-        else:
-            with open(name, 'w') as new_file:
-                new_file.write(name + '\n')
-                new_file.write(password + '\n')
-                new_file.write(age + '\n')
-                new_file.write(gender+ '\n')
-                new_file.write('0')
-                new_file.close()
-                notif.config(fg = 'green', text = "Account created succesfully")
+    file = open(login_name, "r+")
+    file_data = file.read()
+    details = file_data.split('\n')
+    current_balance = details[4]
+    updated_balance = current_balance
+    updated_balance = float(updated_balance) + float(amount.get())
+    file_data = file_data.replace(current_balance, str(updated_balance))
     
-def register():
+    file.seek(0)
+    file.truncate(0)
+    file.write(file_data)
+    file.close()
     
-    # Vars
-    global temp_name, temp_age, temp_gender, temp_password, notif
-    temp_name = StringVar()
-    temp_age = StringVar()
-    temp_gender = StringVar()
-    temp_password = StringVar()
+    current_balance_label.configure(text="Current Balance" + str(updated_balance), text_color = "green")
+    deposit_notif.configure(text = "Balance Updated", text_color = "green")
+
     
     
-    register_screen = Toplevel(master)
-    register_screen.title('Register')
+def deposit():
+    #Vars 
+    global amount 
+    global deposit_notif
+    global current_balance_label
     
-    #Labels
-    Label(master = register_screen, text = "Please enter your details below to register", font = ('Calibri', 12)).grid(row = 0, sticky = N, pady= 10)
-    Label(master = register_screen, text = "Name", font = ('Calibri', 12)).grid(row =1, sticky = W)
-    Label(master = register_screen, text = "Age", font = ('Calibri', 12)).grid(row =2, sticky = W)
-    Label(master = register_screen, text = "Gender", font = ('Calibri', 12)).grid(row =3, sticky = W)
-    Label(master = register_screen, text = "Password", font = ('Calibri', 12)).grid(row =4, sticky = W)
-    notif = Label(master = register_screen, text='', font = ('Calibri', 12))
-    notif.grid(row = 6, sticky = N, pady =10)
+    amount = StringVar()
     
-    #Entires 
-    Entry(register_screen, textvariable= temp_name).grid(row=1, column = 0 )
-    Entry(register_screen, textvariable= temp_age).grid(row=2, column = 0 )
-    Entry(register_screen, textvariable= temp_gender).grid(row=3, column = 0 )
-    Entry(register_screen, textvariable= temp_password, show = "*").grid(row=4, column = 0 )
+    file = open(login_name, "r")
+    file_data = file.read()
+    user_details = file_data.split('\n')
+    details_balance = user_details[4]
     
-    Button(register_screen, text = "Register", command = finish_reg, font = ("Calibri", 12)).grid(row=5, sticky = N, pady=10)
+    #Deposit Screen
     
+    deposit_screen = CTkToplevel(master)
+    deposit_screen.title('Deposit')
+    
+    #Label
+    CTkLabel(deposit_screen, text = "Deposit", font = ('Calibri', 12)).grid(row=0, sticky = N , pady = 10)
+    current_balance_label = CTkLabel(deposit_screen, text = "Current Balance: $" + details_balance, font = ('Calibri', 12))
+    current_balance_label.grid(row=1, sticky = W)
+    
+    CTkLabel(deposit_screen, text = "Amount: ", font = ('Calibri', 12)).grid(row=2, sticky = W)
+    deposit_notif = CTkLabel(deposit_screen, font = ('Calibri', 12))
+    deposit_notif.grid(row=4, sticky = N, pady= 5)
+    
+    #Entry
+    CTkEntry(deposit_screen, textvariable = amount ).grid(row=2, column = 1)
+    
+    #Button
+    CTkButton(deposit_screen, text ="Finish", font = ('Calibri', 12), command = finish_deposit).grid(row=3, sticky = W, pady = 5)
     
 
-master = Tk()
+def finish_withdraw():
+    if withdraw_amount.get() == "":
+        withdraw_notif.config(text = 'Amount is required!', fg = 'red')
+        return 
+    if float(withdraw_amount.get()) <= 0:
+        withdraw_notif.config(text = "Negative currency is not accepted", fg = "red")
+        return 
+    
+    file = open(login_name, "r+")
+    file_data = file.read()
+    details = file_data.split('\n')
+    current_balance = details[4]
+    
+    if float(withdraw_amount.get()) > float(current_balance):
+        withdraw_notif.config(text = "Insuffiecient Funds!", fg= "red")
+        return 
+        
+        
+    updated_balance = current_balance
+    updated_balance = float(updated_balance) - float(withdraw_amount.get())
+    file_data = file_data.replace(current_balance, str(updated_balance))
+    
+    file.seek(0)
+    file.truncate(0)
+    file.write(file_data)
+    file.close()
+    
+    current_balance_label.configure(text="Current Balance" + str(updated_balance), text_color = "green")
+    withdraw_notif.configure(text = "Balance Updated", text_color = "green")
+    
+    
+    
+def withdraw():
+    global withdraw_amount 
+    global withdraw_notif
+    global current_balance_label
+    
+    withdraw_amount = StringVar()
+    
+    file = open(login_name, "r")
+    file_data = file.read()
+    user_details = file_data.split('\n')
+    details_balance = user_details[4]
+    
+    #Deposit Screen
+    
+    withdraw_screen = CTkToplevel(master)
+    withdraw_screen.title('Withdraw')
+    
+    #Label
+    CTkLabel(withdraw_screen, text = "Withdraw", font = ('Calibri', 12)).grid(row=0, sticky = N , pady = 10)
+    current_balance_label = CTkLabel(withdraw_screen, text = "Current Balance: $" + details_balance, font = ('Calibri', 12))
+    current_balance_label.grid(row=1, sticky = W)
+    
+    CTkLabel(withdraw_screen, text = "Amount: ", font = ('Calibri', 12)).grid(row=2, sticky = W)
+    withdraw_notif = CTkLabel(withdraw_screen, font = ('Calibri', 12))
+    withdraw_notif.grid(row=4, sticky = N, pady= 5)
+    
+    #Entry
+    CTkEntry(withdraw_screen, textvariable = withdraw_amount ).grid(row=2, column = 1)
+    
+    #Button
+    CTkButton(withdraw_screen, text ="Finish", font = ('Calibri', 12), command = finish_withdraw).grid(row=3, sticky = W, pady = 5)
+    
+
+master = CTk()
 master.title("Banking App")
+master.geometry('500x500')
 
-#Image import 
-img = Image.open('secure.jpg')
-img = img.resize((150, 150))
-img = ImageTk.PhotoImage(img)
 
 #Labels
-Label(master, text = "Custom Baking Beta", font = ('Calibri', 14)).grid(row=0, sticky = N, pady = 10)
-Label(master, text = "the most secure bank you've probably used", font = ('Calibri', 12)).grid(row=1, sticky = N)
-Label(master, image = img).grid(row=2, sticky = N, pady=15)
+CTkLabel(master, text = "Custom Baking Beta", font = ('Georgia', 35)).place(x=110, y=100)
+CTkLabel(master, text = "The most secure bank you've ever used (NOT)", font = ('Georgia', 15)).place(x=110, y=150)
+
 
 #Buttons
-Button(master, text = "Register", font = ('Calibri', 12), width = 20, command = register).grid(row=3, sticky =N)
-Button(master, text = "Login", font = ('Calibri', 12), width = 20, command = login).grid(row=4, sticky =N, pady= 10)
+CTkButton(master, text = "Register", font = ('Georgia', 18), width = 120, height = 40, command = register).place(x=185, y=220)
+CTkButton(master, text = "Login", font = ('Calibri', 18), width = 120, height= 40, command = login).place(x=185,y=300)
 
 
 
